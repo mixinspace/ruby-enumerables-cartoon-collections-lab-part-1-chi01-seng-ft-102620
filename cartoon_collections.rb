8f058ef53fccefbe30e84fd78494fddd7b2150d0
Islam Madkaur <islammadkaur@gmail.com>
2020-10-26 01:56:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,12 @@
 def greet_characters(array)
-  array.each do |a|
-    puts "Hello "+ a + "!"
-end
+  array.each { |a|
+    puts "Hello #{a}!"   
+
+  }
 
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
-  array.each_with_index do |element, index|
-    index += 1
-    puts "#{index}. #{element}"
-  end
+
 end
